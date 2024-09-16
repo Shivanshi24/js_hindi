d703ad901e87025d027c0112e09916b615882a3d
Shivanshi24 <135115082+Shivanshi24@users.noreply.github.com>
2024-09-16 08:22:43 +0000

    conversion

---
+++ 01_basics/conversion.js
@@ -0,0 +1,62 @@
+//let score = 33
+//let score = "33abc"
+//let score = null
+// let score = undefined
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber)
+
+// 33 => 33
+// 33anc = nan(not a number)
+//true => 1
+//false => 0
+
+//  let isLoggedIn = 1
+// let isLoggedIn = ""
+let isLoggedIn = "shivi"
+
+ let booleanisLoggedIn = Boolean(isLoggedIn)
+ //console.log(booleanisLoggedIn);
+ 
+//  1 => true
+//  0 => false
+// "" = false
+// "any string" = true
+
+// let someNumber = 43
+
+// let stringNumber = String(someNumber)
+// console.log(typeof stringNumber);
+
+// *************************Operations**********************
+
+let vlaue = 4
+let negValue = -vlaue
+console.log(negValue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**2)
+// console.log(2/2)
+// console.log(2%2) for remainder
+
+let str1 = "hello"
+let str2 = " shiva"
+
+let str3 = str1 + str2
+// console.log(str3)
+
+// console.log(1 + "2")
+// console.log("1" + 2)
+// console.log("1" + 2 + 2)
+// console.log(1+ 2 + "2")
+// console.log((3 + 4) * 5 % 3)
+
+console.log(+true)
+console.log(+"")
